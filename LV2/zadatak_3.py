22b74c9ad26364718350550d5bbf80fcc9541821
frankoklepac <klepacfranko1@gmail.com>
2024-03-12 08:28:01 +0100

    Solved task 3

---
+++ LV2/zadatak_3.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = plt.imread('road.jpg')
+
+# a) 
+img = img + 50
+plt.figure()
+plt.imshow(img)
+plt.show()
+
+# b)
+img = plt.imread('road.jpg')
+plt.figure()
+plt.imshow(img[:, img.shape[1]//4:img.shape[1]//2])
+plt.show()
+
+# c) 
+img = plt.imread('road.jpg')
+img = np.rot90(img)
+img = np.rot90(img)
+img = np.rot90(img)
+plt.figure()
+plt.imshow(img)
+plt.show()
+
+# d) 
+img = plt.imread('road.jpg')
+img = img[:, ::-1]
+plt.figure()
+plt.imshow(img)
+plt.show()
+
+
+
