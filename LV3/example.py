6052c703e5c13ea9af5eaa9055d959575fb198c5
frankoklepac <itsmefranko1@gmail.com>
2024-03-18 20:03:35 +0100

    example.py commit

--- LV3/example.py
+++ LV3/example.py
@@ -1,2 +1,16 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+data = pd.read_csv('LV3\data_C02_emission.csv')
+
+data['Make'] = data['Make'].astype('category')
+data['Model'] = data['Model'].astype('category')
+data['Vehicle Class'] = data['Vehicle Class'].astype('category')
+data['Transmission'] = data['Transmission'].astype('category')
+data['Fuel Type'] = data['Fuel Type'].astype('category')
+
+grouped=data.groupby('Cylinders')
+grouped.boxplot( column =['CO2 Emissions (g/km)'])
+data.boxplot ( column =['CO2 Emissions (g/km)'], by='Cylinders')
+plt.show ()
