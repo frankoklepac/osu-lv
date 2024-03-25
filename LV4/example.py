92a204185d29f767dc9800bc5bbcdd8da17e02fd
frankoklepac <itsmefranko1@gmail.com>
2024-03-25 16:37:26 +0100

    LV4 data and example commit

---
+++ LV4/example.py
@@ -0,0 +1,14 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+X, y = datasets.load_diabetes(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+sc = MinMaxScaler()
+X_train_n = sc.fit_transform(X_train)
+X_test_n = sc.transform(X_test)
+
+print(X_train_n)
+print(X_test_n)
